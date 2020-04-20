365214403fd29a0a6d4b240c87437f7beadaeadf
suniel182 <suniel182@gmail.com>
2020-04-20 14:56:11 +0545

    Graphics updated, list of sites updated. Ready for the prime time

--- app.js
+++ app.js
@@ -3,14 +3,25 @@
 const newsReader = {
 	body: document.getElementsByTagName('body'),
 	ads: document.querySelectorAll(
-		'img[src$=".gif"], a.static-sponsor, a[href*="ncell"], a[href*="esewa"], a[href*="khalti"], a[href*="imepay"], a[href*="bank"], a[href*="dishhome"], a[href*="insurance"]'
+		'img[src*=".gif"], a.static-sponsor, a[href*="ncell"], a[href*="esewa"], a[href*="khalti"], a[href*="imepay"], a[href*="bank"], a[href*="dishhome"], a[href*="insurance"], img[src*="ad-"], img[src*="-ad"], img[src*="ad."], img[src*="_ad"], img[src*="ad_"], *[id="ad"], *[class="ad"], *[id*="_ad"], *[class*="_ad"], *[id*="ad_"], *[class*="ad_"], *[id*="-ad"], *[class*="-ad"], *[id*="ad-"], *[class*="ad-"], *[id*="bigyapan_"], *[class*="_bigyapan"], *[id*="bigyapan-"], *[class*="-bigyapan"], *[id*="_bigyapan"], *[class*="_bigyapan"], *[id*="-bigyapan"], *[class*="-bigyapan"], *[class*="advertisement"], *[class*=" ad "]'
 	),
 	site: location.host,
 	adPanel: document.createElement('div'),
 
 	init() {
-		console.log(this.ads);
-		Array.from(this.ads).map((ad) => ad.parentNode.removeChild(ad));
+		Array.from(this.ads).map((ad) => {
+			if (ad.querySelector('header') != null) {
+				ad.parentNode.prepend(ad.querySelector('header'));
+			}
+			if (ad.querySelector('footer') != null) {
+				ad.parentNode.appendChild(ad.querySelector('footer'));
+			}
+			if (ad.querySelector('.content') != null) {
+				ad.parentNode.prepend(ad.querySelector('.content'));
+			}
+			ad.remove();
+			// ad.parentNode.removeChild(ad);
+		});
 	},
 
 	appendAdPanel() {
@@ -35,3 +46,18 @@ const newsReader = {
 newsReader.log();
 newsReader.appendAdPanel();
 newsReader.init();
+
+//Run after two seconds to remove any post load scripts
+setTimeout(function () {
+	newsReader.log();
+	newsReader.appendAdPanel();
+	newsReader.init();
+	clearInterval(interval);
+}, 2000);
+
+//create 30 seconds interval to hide regenerated ads
+var interval = setInterval(function () {
+	newsReader.log();
+	newsReader.appendAdPanel();
+	newsReader.init();
+}, 30000);
